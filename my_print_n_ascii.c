45ba527c6324d136879f24d2c84a62ddfdb4f002
MasterClavier <154695316+MasterClavier@users.noreply.github.com>
2024-05-25 11:44:44 +0000

    n ascii

--- my_print_n_ascii.c
+++ my_print_n_ascii.c
@@ -1,3 +1,5 @@
+#include "stdio.h"
+
 int print_char(char);
 
 void my_print_n_ascii(int howMany)
@@ -5,11 +7,11 @@ void my_print_n_ascii(int howMany)
     int n = 33;
     if (howMany >= 94)
     {
-        return 0;
+        return;
     }
     else 
     {
-        while (n <= howMany-33)
+        while (n <= howMany+33)
         {
             print_char(n);
             n +=1;
